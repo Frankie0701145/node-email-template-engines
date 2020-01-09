2754ae2c8ee364d6c93355fb8882534c170b5431
Francis Njuguna <coulsorfrancois@gmail.com>
2020-01-09 12:28:54 +0300

    Add code to retrieve the file extension from the templatePath.

---
+++ dist/index.js
@@ -0,0 +1,34 @@
+
+
+
+const emailTemplateEngine = (templatePath, passObject)=>{
+
+    //the templatePath is the absolute path to the view.
+    //the passObject is the data to be passed to the view.
+
+
+    //retrieve the view extension from the view templatePath
+    //use the file extension to compile the view to html
+
+    let pathArray = templatePath.split('/'); //split the path into an array
+
+    let file = pathArray[pathArray.length-1]; //retrieve the file
+    let fileArray = file.split('.'); //split the file into file name and extension
+    let fileExtension = fileArray[fileArray.length-1]; //get the file extension
+
+    //switching between the different fileExtension
+    switch(fileExtension){
+        
+        //in case it is EJS
+        case 'ejs':
+            break;
+        
+        default:
+            let error = new Error();
+            error.message = `For now the view with ${fileExtension} is not supported only ejs is supported.`;
+            throw(error);
+    }
+
+}
+
+module.exports =  emailTemplateEngine;
