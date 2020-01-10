733cd4e0563d95ff212e052d34635fadaecedf58
Francis Njuguna <coulsorfrancois@gmail.com>
2020-01-10 15:56:24 +0300

    Successfully tested that an error is thrown if the file path of the view is not found.

--- dist/index.js
+++ dist/index.js
@@ -1,41 +1,42 @@
+let ejsCompiler = require('./helpers/ejsCompiler');
+
 //helper functions
 let errMessageTemplatePath = 'templatePath argument is required';
 let errMessagePassObject = 'passObject argument is required';
 let required = (message)=>{
-    throw(new Error(message));
+    let err = new Error(message);
+    console.error(err);
+    throw(err);
 };
 
 
 
 
-const emailTemplateEngine = (templatePath=required(errMessageTemplatePath), passObject=required(errMessagePassObject))=>{
+const emailTemplateEngine = async (templatePath=required(errMessageTemplatePath), passObject=required(errMessagePassObject))=>{
 
-    //the templatePath is the absolute path to the view.
-    //the passObject is the data to be passed to the view.
+   try{
+        //the templatePath is the absolute path to the view.
+        //the passObject is the data to be passed to the view.
+        //retrieve the view extension from the view templatePath
+        //use the file extension to compile the view to html
 
+        let pathArray = templatePath.split('/'); //split the path into an array
 
-    //retrieve the view extension from the view templatePath
-    //use the file extension to compile the view to html
+        let file = pathArray[pathArray.length-1]; //retrieve the file
+        let fileArray = file.split('.'); //split the file into file name and extension
+        let fileExtension = fileArray[fileArray.length-1]; //get the file extension
 
-    let pathArray = templatePath.split('/'); //split the path into an array
-
-    let file = pathArray[pathArray.length-1]; //retrieve the file
-    let fileArray = file.split('.'); //split the file into file name and extension
-    let fileExtension = fileArray[fileArray.length-1]; //get the file extension
-
-    //switching between the different fileExtension
-    switch(fileExtension){
-
-        //in case it is EJS
-        case 'ejs':
-            break;
-        
-        default:
-            let error = new Error();
-            error.message = `For now the view with ${fileExtension} is not supported only ejs is supported.`;
-            throw(error);
-    }
-
+        if(fileExtension === 'ejs'){
+            let str = await ejsCompiler(templatePath, passObject);
+            return str;
+        }else{
+            let err = new Error(`For now the view with ${fileExtension} is not supported only ejs is supported.`);
+            throw(err);
+        }
+   }catch(err){ 
+       console.error(err);
+       throw(err);
+   }
 }
 
 
--- test/ejsTest.js
+++ test/ejsTest.js
@@ -1,45 +1,60 @@
 let path = require('path');
 let emailTemplateEngine = require('../dist/index');
 let chai = require('chai');
+let chaiAsPromised = require('chai-as-promised');
 let expect =chai.expect;
+chai.use(chaiAsPromised);
+
 
 describe('Email Template Engine Test', ()=>{
 
     let ejsTemplatePath;
-    let passObject = {
-        firstName: 'Francis',
-        lastName: 'Njuguna'
-    };
+    let passObject;
 
     before(()=>{
         ejsTemplatePath = path.resolve(__dirname, './views/ejsTemplate.ejs');
+        passObject = {
+            firstName: 'Francis',
+            lastName: 'Njuguna'
+        };
     });
 
 
-    it('Expect an error to be thrown if the templatePath argument is not supplied', ()=>{
-        expect(()=>{
-            emailTemplateEngine();
-        }).to.throw('templatePath argument is required');
+    it('Expect an error to be thrown if the templatePath argument is not supplied', async ()=>{    
+        await expect(emailTemplateEngine())
+                 .to.be.rejectedWith('templatePath argument is required');
     });
 
-    it('Expect an error to be thrown if the passObject argument is not supplied', ()=>{
-        expect(()=>{
-            emailTemplateEngine(ejsTemplatePath);
-        }).to.throw('passObject argument is required');
+    it('Expect an error to be thrown if the passObject argument is not supplied', async ()=>{
+        await expect(emailTemplateEngine(ejsTemplatePath))
+                 .to.be.rejectedWith('passObject argument is required');
     });
 
 
-    it('Expect to get an error if the file extension of the view is not supported', ()=>{
+    it('Expect to get an error if the file extension of the view is not supported', async ()=>{
             let testEjsPath = path.resolve('./views/l.pug');
-            expect(()=>{
-                emailTemplateEngine(testEjsPath);
-            }).to.throw().with.property('message');
+            await expect(emailTemplateEngine(testEjsPath, passObject))
+                   .to.be.rejectedWith('For now the view with pug is not supported only ejs is supported.');
     });
 
-    it('Expect to get back the back html str with the correct ejs template path', async ()=>{
-        console.log(ejsTemplatePath);
-        let str = await emailTemplateEngine(ejsTemplatePath, passObject);
-        console.log('The html string');
-        console.log(str);
+    it('Expect to get an error if the file path of the view is wrong', async ()=>{
+        let testEjsPath = path.resolve('./views/l.ejs');
+        let expectedError = {
+            errno: -2,
+            syscall: 'open',
+            code: 'ENOENT',
+            path: '/home/francis/PersonalProjects/packages/node-email-template-engines/views/l.ejs'
+        }
+        await expect(emailTemplateEngine(testEjsPath, passObject))
+               .to.be.rejectedWith(expectedError);
     });
+
+    // it('Expect to get back the back html str with the correct ejs template path', async ()=>{
+
+    //     let strHtml = await emailTemplateEngine(ejsTemplatePath, passObject);
+
+    //     console.log(strHtml);
+    // });
+
+ 
 });
