5caf8e0881025353ab576fbbe6264af41289a8ae
Francis Njuguna <coulsorfrancois@gmail.com>
2020-01-10 16:56:09 +0300

    Test that the returned resolved promise contains a string.

--- test/ejsTest.js
+++ test/ejsTest.js
@@ -20,24 +20,24 @@ describe('Email Template Engine Test', ()=>{
     });
 
 
-    it('Expect an error to be thrown if the templatePath argument is not supplied', async ()=>{    
+    it('Expect an error to be thrown if the templatePath argument is not supplied.', async ()=>{    
         await expect(emailTemplateEngine())
                  .to.be.rejectedWith('templatePath argument is required');
     });
 
-    it('Expect an error to be thrown if the passObject argument is not supplied', async ()=>{
+    it('Expect an error to be thrown if the passObject argument is not supplied.', async ()=>{
         await expect(emailTemplateEngine(ejsTemplatePath))
                  .to.be.rejectedWith('passObject argument is required');
     });
 
 
-    it('Expect to get an error if the file extension of the view is not supported', async ()=>{
+    it('Expect to get an error if the file extension of the view is not supported.', async ()=>{
             let testEjsPath = path.resolve('./views/l.pug');
             await expect(emailTemplateEngine(testEjsPath, passObject))
                    .to.be.rejectedWith('For now the view with pug is not supported only ejs is supported.');
     });
 
-    it('Expect to get an error if the file path of the view is wrong', async ()=>{
+    it('Expect to get an error if the file path of the view is wrong.', async ()=>{
         let testEjsPath = path.resolve('./views/l.ejs');
         let expectedError = {
             errno: -2,
@@ -48,17 +48,18 @@ describe('Email Template Engine Test', ()=>{
         await expect(emailTemplateEngine(testEjsPath, passObject))
                .to.be.rejectedWith(expectedError);
     });
-    it.only('Expect that the passObject is accessible to the view', async ()=>{
+
+    it('Expect that the passObject is accessible to the view template if the correct arguments are passed.', async ()=>{
         let strHtml = await emailTemplateEngine(ejsTemplatePath, passObject);
         expect(strHtml).to.include(passObject.firstName);
         expect(strHtml).to.include(passObject.lastName);
     });
-    // it('Expect to get back the back html str with the correct ejs template path', async ()=>{
 
-    //     let strHtml = await emailTemplateEngine(ejsTemplatePath, passObject);
-
-    //     console.log(strHtml);
-    // });
+    it('Expect to get back html str if the correct arguments are passed.', async ()=>{
+        let strHtml = await emailTemplateEngine(ejsTemplatePath, passObject);
+        let type = typeof(strHtml);
+        expect(type).to.be.equal('string');
+    });
 
  
 });
